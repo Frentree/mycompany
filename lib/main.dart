84ac934e8a68b54e1422869774d21c74bc1c95d8
전준현 <info@frentree.com>
2021-06-18 17:30:05 +0900

    완료 : (FCM)Xcode wetting for FCM / 다음 : (FCM)FCM 기능 추가 & 연차 계산

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/services.dart';
-import 'package:mycompany/public/style/color.dart';
+import 'package:mycompany/public/function/fcm/fcm_init.dart';
 import 'package:mycompany/run_app/view/permission_request_view.dart';
 import 'package:mycompany/run_app/view/splash_view_white.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -21,6 +21,9 @@ void main() async {
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
   bool a = await checkPermissionFunction();
+
+  ///Initialize configurations for Firebase Cloud Messagings
+  FcmInit();
 
   runApp(
     EasyLocalization(
