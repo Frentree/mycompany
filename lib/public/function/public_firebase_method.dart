c36b4e51f60dbe101cf08e8e40a07e54939a8837
전준현 <info@frentree.com>
2021-07-02 16:14:01 +0900

    완료 : [연차] 사용량 계산 / 다음 : [와이파이] 기능 개발

--- lib/public/function/public_firebase_method.dart
+++ lib/public/function/public_firebase_method.dart
@@ -1,16 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:mycompany/login/model/user_model.dart';
+import 'package:mycompany/public/format/date_format.dart';
 import 'package:mycompany/public/function/fcm/alarmModel.dart';
 
 class PublicFirebaseMethods {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final DateFormatCustom _dateFormatCustom = DateFormatCustom();
 
   PublicFirebaseMethods.settings(
       {persistenceEnabled: true, cacheSizeBytes: -1});
 
-  Future<List<String>> getTokensFromUsers(
-      UserModel user, String? companyCode, List users) async {
+  Future<List<String>> getTokensFromUsers(UserModel user, String? companyCode,
+      List users) async {
     List<String> _tokens = [];
 
     users.forEach((_user) async {
@@ -36,13 +38,16 @@ class PublicFirebaseMethods {
     return _tokens;
   }
 
-  Future<void> saveAlarmDataFromUsersThenSend(
-      UserModel user, String? companyCode, List users, var payload) async {
-    HttpsCallable callFcm = FirebaseFunctions.instance.httpsCallable('sendFcmNew');
+  Future<void> saveAlarmDataFromUsersThenSend(UserModel user,
+      String? companyCode, List users, var payload) async {
+    HttpsCallable callFcm =
+    FirebaseFunctions.instance.httpsCallable('sendFcmNew');
 
     users.forEach((_userMail) async {
       print("user mail : $_userMail");
-      int _alarmId = DateTime.now().hashCode;
+      int _alarmId = DateTime
+          .now()
+          .hashCode;
 
       /// 알람 데이터 저장
       var _docRef = _firestore
@@ -103,9 +108,8 @@ class PublicFirebaseMethods {
     });
   }
 
-  Future<void> setAlarmReadToTrue(
-      String? companyCode, String? mail, String alarmId) async {
-
+  Future<void> setAlarmReadToTrue(String? companyCode, String? mail,
+      String alarmId) async {
     try {
       _firestore
           .collection("company")
@@ -115,8 +119,40 @@ class PublicFirebaseMethods {
           .collection("alarm")
           .doc(alarmId)
           .set({"read": true});
-    } catch(e) {
+    } catch (e) {
       print(e.toString());
     }
+  }
+
+  Future<double> usedVacationWithDuration(String? companyCode, String? mail,
+      DateTime start, DateTime end) async {
+    double result = 0.0;
+
+    Timestamp _start = _dateFormatCustom.changeDateTimeToTimeStamp(
+        dateTime: start);
+    Timestamp _end = _dateFormatCustom.changeDateTimeToTimeStamp(dateTime: end);
+
+    late QuerySnapshot _querySnapshots;
+
+    try {
+      _querySnapshots = await _firestore
+          .collection("company")
+          .doc(companyCode)
+          .collection("work")
+          .where("createUid", isEqualTo: mail)
+          .where("startTime", isLessThanOrEqualTo: _end)
+          .where("startTime", isGreaterThanOrEqualTo: _start)
+          .where("type", whereIn: ['연차', '반차'])
+          .orderBy("startTime", descending: false)
+          .get();
+
+      _querySnapshots.docs.forEach((element) {
+        result += (element.data() as dynamic)['type'] == '연차'? 1 : 0.5;
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+
+    return result;
   }
 }
--- lib/public/widget/main_menu.dart
+++ lib/public/widget/main_menu.dart
@@ -55,7 +55,13 @@ Widget getMainCircularMenu(
         CircularMenuItem(
             icon: Icons.star,
             color: navigator != 'good' ? Colors.blue : Colors.black38,
-            onTap: () => TotalVacation('2018.10.10', 0),
+            onTap: () {
+              testForUsedVacation();
+            }
+            // onTap: ()  {
+            //   UsedVacation('0S9YLBX',
+            //       'jun@frentree.com', '2018.10.23', false);}
+            // onTap: () => TotalVacation('2018.10.10', false, 0),
             // onTap: () => navigator != 'schedule'
             //     ? _reprository.mainNavigator(
             //         context: context, navigator: ScheduleView(), isMove: false)
@@ -71,3 +77,4 @@ Widget getMainCircularMenu(
                 : {}),
       ]);
 }
+
