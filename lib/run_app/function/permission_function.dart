bb6b3f754d79f9e8c55efb0b01d968c4fb7d89ad
전준현 <info@frentree.com>
2021-07-07 16:36:49 +0900

    WIFI] wifi on iOS 출근 로직에 마이그레이션 / 다음 : 통합 테스트

--- lib/run_app/function/permission_function.dart
+++ lib/run_app/function/permission_function.dart
@@ -5,24 +5,13 @@ import 'package:flutter/foundation.dart' as foundation;
 import 'package:location/location.dart' as loc;
 
 Future<bool> requestPermissionFunction() async {
-  print('requestPermissionFunction has be executed');
   Map<Permission, PermissionStatus> permissionStatus = await [
     Permission.location,
   ].request();
 
-  // print(Permission.location);
-  // print(await Permission.location.status);
-  // print(Permission.locationWhenInUse);
-  // print(await Permission.locationWhenInUse.status);
-  // print(Permission.locationAlways);
-  // print(await Permission.locationAlways.status);
-  // print(permissionStatus.values);
-
   if(permissionStatus.values.every((element) => element.isGranted)){
-    print('permission status is true');
     return true;
   }
-  print('request permission function finished');
 
   return false;
 }
@@ -36,37 +25,42 @@ Future<bool> checkPermissionFunction() async {
 
   else{
     if (foundation.defaultTargetPlatform == foundation.TargetPlatform.iOS) {
-      print('TargetPlatform is iOS');
-      loc.Location location = new loc.Location();
-
-      bool _serviceEnabled;
-      loc.PermissionStatus _permissionGranted;
-      loc.LocationData _locationData;
-
-      _serviceEnabled = await location.serviceEnabled();
-      if (!_serviceEnabled) {
-        _serviceEnabled = await location.requestService();
-        if (!_serviceEnabled) {
-          print('!_serviceEnabled');
-          return false;
-        }
-      }
-
-      _permissionGranted = await location.hasPermission();
-      if (_permissionGranted == loc.PermissionStatus.denied) {
-        _permissionGranted = await location.requestPermission();
-        if (_permissionGranted != loc.PermissionStatus.granted) {
-          print('_permissionGranted != loc.PermissionStatus.granted');
-          return false;
-        }
-      } else {
-        print('HAS PERMISSION');
-        return true;
-      }
-
-      _locationData = await location.getLocation();
-      print('_locationData = $_locationData');
+      return checkIOSPermission();
     }
     return false;
   }
+}
+
+Future<bool> checkIOSPermission() async {
+  print('TargetPlatform is iOS');
+  loc.Location location = new loc.Location();
+
+  bool _serviceEnabled;
+  loc.PermissionStatus _permissionGranted;
+  loc.LocationData _locationData;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      print('!_serviceEnabled');
+      return false;
+    }
+  }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == loc.PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != loc.PermissionStatus.granted) {
+      print('_permissionGranted != loc.PermissionStatus.granted');
+      return false;
+    }
+  } else {
+    print('This iOS device HAS PERMISSION');
+    return true;
+  }
+
+  _locationData = await location.getLocation();
+  print('_locationData = $_locationData');
+  return false;
 }
--- lib/run_app/view/auth_view.dart
+++ lib/run_app/view/auth_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mycompany/attendance/view/attendance_dashboard_view.dart';
 import 'package:mycompany/login/model/employee_model.dart';
 import 'package:mycompany/login/view/force_sign_out_view.dart';
 import 'package:mycompany/login/view/join_company_success_view.dart';
@@ -7,7 +6,6 @@ import 'package:mycompany/login/view/reject_join_company_approval_view.dart';
 import 'package:mycompany/login/view/sign_in_view.dart';
 import 'package:mycompany/login/view/wait_join_company_approval_view.dart';
 import 'package:mycompany/public/provider/device_Info_provider.dart';
-import 'package:mycompany/main.dart';
 import 'package:mycompany/public/provider/employee_Info_provider.dart';
 import 'package:mycompany/run_app/function/auto_check_on_work_function.dart';
 import 'package:mycompany/schedule/view/schedule_view.dart';
